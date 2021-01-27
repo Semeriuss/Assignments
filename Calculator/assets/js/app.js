a07ae5b2b99b6a32174c30c795f86f0b27475c8d
Semere Habtu <se.semere.habtu@gmail.com>
2021-01-28 01:09:58 +0300

    Created subtraction function by DOM manipulation

--- Calculator/assets/js/app.js
+++ Calculator/assets/js/app.js
@@ -1,3 +1,50 @@
+const result = document.querySelector('.result'); //screen input text field
+const result1 = document.querySelector('#result1'); //screen input text field
+const result2 = document.querySelector('#result2'); //screen input text field
+const operation = document.querySelectorAll(".global"); //main buttons - operands and operators
+var display = document.querySelector('.header');
+const buttonArray = Array.from(operation); //change nodelist into array
+const addButton = buttonArray[2];
+const subButton = buttonArray[5];
+const mulButton = buttonArray[1];
+const divButton = buttonArray[4];
+const perButton = buttonArray[3];
+const sqrtButton = buttonArray[0];
+console.log(buttonArray);
+
+//addition button
+// addButton.addEventListener("click", addResults);
+
+//subtraction button
+subButton.addEventListener("click", subResults);
+
+//multiplication button
+// mulButton.addEventListener("click", mulResults);
+
+//division button
+// divButton.addEventListener("click", divResults);
+
+//percent button
+// perButton.addEventListener("click", perResults);
+
+//sqrt button
+// sqrtButton.addEventListener("click", sqrtResults);
+
+// .split(/[ ,]+/)
+//add new task funtion definition
+function subResults(){
+    var num1 = result1.value;
+    var num2 = result2.value;
+    display.textContent = subtract(num1,num2);
+}
+
+function subResults(){
+    var num1 = result1.value;
+    var num2 = result2.value;
+    display.textContent = subtract(num1,num2);
+}
+
+
 function add(arr){
     var sum = 0;
     res = arr.forEach(function(values, index) {
@@ -20,9 +67,29 @@ function subtract(num1, num2){
 
 function divide(num1, num2) {
     if(num2 == 0){
-        return isZero = true;
+        return undefined = true;
     }
     else{
         return (num1/num2).toPrecision(3);
     }  
 }
+
+function power(num, exp){
+    if(num==0 && exp<0){
+        return undefined = true;
+    }else{
+        return Math.pow(num, exp);
+    }
+}
+
+function sqrt(num){
+    if (num<0){
+        return undefined = true;
+    }else{
+        return Math.sqrt(num);
+    }
+}
+function log(num, base){
+    return Math.log(num, base);
+}
+
