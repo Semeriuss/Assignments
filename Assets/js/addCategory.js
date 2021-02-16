89fb5dc4e9ea4d787ad806c73ba1d63cec93b809
Amanuel <amanueldebebe1000@gmail.com>
2021-02-16 09:26:05 +0300

    integrated addInsurance function with the form

--- Assets/js/addCategory.js
+++ Assets/js/addCategory.js
@@ -1,33 +1,29 @@
-function addMainCat(title) {
+function addMainCat(main) {
 	return db
 		.transaction('rw', db.main_category, function() {
 			db.main_category
-				.add(title)
+				.add(main)
 				.then((val) => {
-					// console.log("Worked.." + val);
-					return true;
+					console.log('Worked..', val);
+					// return true;
 				})
 				.catch((val) => {
-					console.log('Some Error Happened' + val);
-					return false;
+					console.log('Some Error Happened', val);
+					// return false;
 				});
 		})
-		.catch(function(e) {
+		.catch((e) => {
 			console.error(e.stack);
 		});
 }
 
-var add_btn = document.getElementById('add_insurance');
+var form = document.querySelector('#form');
 var insurance_input = document.getElementById('insurance_name');
 var error_label = document.getElementById('error_display');
 
-function submitted() {
-	console.log('CLICKED');
-	if (insurance_input.value == '') {
-		insurance_input.style.borderColor = 'red';
-		error_label.value = 'Required Field';
-		error_label.style.color = 'red';
-	} else {
-		console.log(insurance_input.value);
-	}
-}
+form.addEventListener('submit', () => {
+	console.log(insurance_input.value);
+	addMainCat({ name: insurance_input.value });
+});
+
+// addMainCat({ name: 'Life Insurance' });
