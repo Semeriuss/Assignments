e00e1cf5692a048f4cec42ef0a4970822ccf833b
Semere Habtu <se.semere.habtu@gmail.com>
2021-02-16 21:58:37 +0300

    Added bootstrap modal for editing subcategory

--- Assets/js/subCat.js
+++ Assets/js/subCat.js
@@ -26,11 +26,11 @@ function addSubDemo(title) {
 			console.error(e.stack);
 		});
 }
-// addSubDemo({name: "Health", maincat: "Life-Insurance", date: new Date().toUTCString()});
-// addSubDemo({name: "Motor", maincat: "Property-Insurance", date: new Date().toUTCString()});
-// addSubDemo({name: "Cycle", maincat: "Fire-Insurance", date: new Date().toUTCString()});
-// addSubDemo({name: "Travel", maincat: "Liability-Insurance", date: new Date().toUTCString()});
-// addSubDemo({name: "Mobile", maincat: "Guarantee-Insurance", date: new Date().toUTCString()});
+addSubDemo({name: "Health", maincat: "Life-Insurance", date: new Date().toUTCString()});
+addSubDemo({name: "Motor", maincat: "Property-Insurance", date: new Date().toUTCString()});
+addSubDemo({name: "Cycle", maincat: "Fire-Insurance", date: new Date().toUTCString()});
+addSubDemo({name: "Travel", maincat: "Liability-Insurance", date: new Date().toUTCString()});
+addSubDemo({name: "Mobile", maincat: "Guarantee-Insurance", date: new Date().toUTCString()});
 
 const tableSubRow = document.querySelector('.subRowData');
 function displaySubCategory() {
@@ -91,7 +91,8 @@ function insertSubElement(objText) {
 	td2.className = 'date';
 	td2.appendChild(document.createTextNode(moment(objText.date).format('YYYY-MM-DD')));
 	const link = document.createElement('a');
-	link.innerHTML = `<a href="edit.html"><i class="fas fa-edit"></i></a>`;
+	link.innerHTML = `<a href="#" data-toggle="modal" data-target="#editModal"><i class ="fas fa-edit"></i></a>`;
+	link.innerHTML += modal;
 	const td3 = document.createElement('td');
 	td3.className = 'editLink';
 	td3.appendChild(link);
@@ -102,5 +103,46 @@ function insertSubElement(objText) {
 	tr.appendChild(td3);
 	tableSubRow.appendChild(tr);
 }
+
+var modal = `
+<div id="editModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="editModalLabel">Edit</h4>  
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+
+				<!-- Start of Editing Form -->
+				<form class="col-md-12 bg-secondary" id="form">
+					<p class="text-white">Edit the following Information</p>
+					<hr style="background-color: aliceblue;">
+					<div class="form-group mt-4">
+						<label class="text-white" for="category">Category: *</label><br>
+						<select style="width: 24.5rem;" name="category" id="cat">
+			
+						</select>
+					</div>
+					<div class="form-group mt-4">
+						<label class="text-white" for="name">Sub Category Name: *</label>
+						<input style="width: 24.5rem;" type="text" required id="name" class="form-control bg-light">
+					</div>
+					<div class="row">
+						<div class="col-md-12 mb-4 mt-2">
+							<button class="btn btn-info px-5 p-2 mb-2">Save Changes</button>
+						</div>
+					</div>
+			  	</form>
+				  <!-- End of Editing Form -->
+
+			</div>
+		</div>
+	</div>
+</div>
+`
 
 displaySubCategory();
