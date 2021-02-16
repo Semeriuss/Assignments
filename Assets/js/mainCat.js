1b7c0c17b718f70e5f0b7cbf05ab4d51ead3c8e9
Semere Habtu <se.semere.habtu@gmail.com>
2021-02-16 21:28:05 +0300

    Added a bootstrap modal feature for the edit link

--- Assets/js/mainCat.js
+++ Assets/js/mainCat.js
@@ -18,7 +18,7 @@ function addDemo(title) {
 }
 // addDemo({ name: 'Life-Insurance', date: new Date().toISOString() });
 // addDemo({ name: 'Property-Insurance', date: new Date().toLocaleDateString() });
-// addDemo({ name: 'Fire-Insurance', date: new Date().toLocaleTimeString() });
+// addDemo({ name: 'Fire-Insurance', date: new Date().toLocaleDateString() });
 // addDemo({ name: 'Liability-Insurance', date: new Date().toString() });
 // addDemo({ name: 'Guarantee-Insurance', date: new Date().toUTCString() });
 
@@ -52,7 +52,8 @@ function insertElement(objText) {
 	td2.className = 'date';
 	td2.appendChild(document.createTextNode(moment(objText.date).format('YYYY-MM-DD')));
 	const link = document.createElement('a');
-	link.innerHTML = `<a href="edit.html"><i class ="fas fa-edit"></i></a>`;
+	link.innerHTML = `<a href="#" data-toggle="modal" data-target="#editModal"><i class ="fas fa-edit"></i></a>`;
+	link.innerHTML += modal;
 	const td3 = document.createElement('td');
 	td3.className = 'editLink';
 	td3.appendChild(link);
@@ -63,4 +64,39 @@ function insertElement(objText) {
 	tableRow.appendChild(tr);
 }
 
+var modal = `
+<div id="editModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="editModalLabel">Edit</h4>  
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+
+				<!-- Start of Editing Form -->
+					<form class="col-md-12 bg-secondary" id="form">
+					  <p class="text-white">Edit the following Information</p>
+					  <hr style="background-color: aliceblue;">
+					  <div class="form-group mt-4">
+						<label class="text-white" for="name">Category: </label>
+						<input style="width: 24.5rem;" type="text" required id="insurance_name" class="form-control bg-light">
+						<label for="name" id="error_display"></label>
+					  </div>
+					  <div class="row">
+						<div class="col-md-12 mb-4 mt-2">
+						  <button class="btn btn-info px-5 p-2 mb-2" id="edit_insurance" type="submit">Save Changes</button>
+						</div>
+					  </div>
+					</form>
+				  <!-- End of Editing Form -->
+
+			</div>
+		</div>
+	</div>
+</div>
+`
 displayMainCategory();
