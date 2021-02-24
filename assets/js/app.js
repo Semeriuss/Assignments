6cf66475b76632858c080b212ff17c4bf370fd23
Semere Habtu <se.semere.habtu@gmail.com>
2021-02-24 22:17:58 +0300

    Equal image size, tentative font and background

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,13 +55,14 @@ function loadDataNew() {
             let display = '';
             countries.forEach(function(country) {
                 display += `
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <img height="40" width="40" class="card-img-top img-fluid" src="${country.flag}" alt="">
-                            <h4 class="card-title">${country.name}</h4>
-                            <h6 class="card-title">${country.capital}</h6>
-                            <p class="card-text">Lorem ipsum dolor sit</p>
+                <div class="col-md-4 mr-1 mb-4 border-dark">
+                    <div class="card-header"><h6 style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;" class="card-text">${country.region}</h6></div>
+                        <div class="card">
+                            <div class="card-body bg-info">
+                                <img style="width: 20rem; height: 10rem;" class="card-img-top img-fluid" src="${country.flag}" alt="">
+                                <h4 style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;" class="card-title mt-2">${country.name}</h4>
+                                <h5 style="font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;" class="card-title">${country.capital}</h5>
+                            </div>
                         </div>
                     </div>
                 </div>
