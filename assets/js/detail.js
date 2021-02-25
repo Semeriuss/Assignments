ae65451f040e29a53309f34ff44599ff8f770edf
Amanuel <amanueldebebe1000@gmail.com>
2021-02-25 21:09:00 +0300

    layout  of second page created

---
+++ assets/js/detail.js
@@ -0,0 +1,21 @@
+//UI variables
+const main = document.querySelector('.main');
+
+//read from q string
+const urlParams = new URLSearchParams(window.location.search);
+const id = Number(urlParams.get('id'));
+
+
+
+
+
+function display (country) {
+
+    let html = `
+    
+    `
+
+
+
+
+}
