25e2efa7f7f68d58ea51a919a9b4c4d1757af800
Biniyam <se.biniyam.abiy@gmail.com>
2021-03-04 10:38:03 +0300

    the functionality that lets the user views his policies approved by the administrator added through the user history.html page.

---
+++ Assets/js/userHistory.js
@@ -0,0 +1,74 @@
+const tableRow = document.querySelector('.HistoryRowData');
+const userName = document.querySelector('#USER');
+
+
+
+function addDemo(title) {
+	return db
+		.transaction('rw', db.approved_policies, function() {
+			db.approved_policies
+				.add(title)
+				.then((val) => {
+					console.log("Worked.." + val);
+					return true;
+				})
+				.catch((val) => {
+					console.log('Some Error Happened' + val);
+					return false;
+				});
+		})
+		.catch(function(e) {
+			console.error(e.stack);
+		});
+}
+
+
+addDemo({ uname: 'checking1', approved_policy: "first policy"});
+addDemo({ uname: 'checking1', approved_policy: "second policy"});
+addDemo({ uname: 'checkandLovely', approved_policy: "third policy"});
+
+
+
+
+
+
+function displayUserHistory() {
+	return db
+		.transaction('r', db.approved_policies, function() {
+			db.approved_policies
+				.each((element) => {
+                    if (element.uname == userName.value) {
+                        insertElement(element)
+                    }
+                })
+				.then((res) => {
+					return true;
+				})
+				.catch((res) => {
+					return false;
+				});
+		})
+		.catch(function(e) {
+			console.error(e.stack);
+		});
+}
+
+
+
+
+function insertElement(approved_policy) {
+	const tr = document.createElement('tr');
+	const td0 = document.createElement('td');
+	td0.className = 'approvedPolicyId';
+	td0.appendChild(document.createTextNode(approved_policy.id));
+	const td = document.createElement('td');
+	td.className = 'approvedPolicyName';
+	td.appendChild(document.createTextNode(approved_policy.approved_policy));
+	tr.appendChild(td0);
+	tr.appendChild(td);
+	tableRow.appendChild(tr);
+}
+
+setTimeout(() => {
+    displayUserHistory();
+}, 0);
